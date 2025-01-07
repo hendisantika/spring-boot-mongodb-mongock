c7f635c32d72ab59459e1765a6a9554e4b01696c
hendisantika <hendisantika@yahoo.co.id>
2025-01-07 09:28:06 +0700

    Add DatabaseChangeLog🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/mongock/config/DatabaseChangeLog.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.mongock.config;
+
+import com.github.cloudyrock.mongock.ChangeLog;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mongock-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/01/25
+ * Time: 09.27
+ * To change this template use File | Settings | File Templates.
+ */
+@ChangeLog
+public class DatabaseChangeLog {
+}
