3e5164cb17334b1ba9004293e4e338b550287be9
hendisantika <hendisantika@yahoo.co.id>
2025-01-07 09:30:06 +0700

    Add getExpenseByName in ExpenseController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/mongock/controller/ExpenseController.java
+++ src/main/java/id/my/hendisantika/mongock/controller/ExpenseController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -48,5 +49,8 @@ public class ExpenseController {
         return ResponseEntity.ok(expenseService.getAllExpenses());
     }
 
-
+    @GetMapping("/{name}")
+    public ResponseEntity getExpenseByName(@PathVariable String name) {
+        return ResponseEntity.ok(expenseService.getExpense(name));
+    }
 }
