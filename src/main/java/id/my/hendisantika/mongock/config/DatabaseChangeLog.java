5a94164e5de18cc716aaa46725d1a8c4b52765f8
hendisantika <hendisantika@yahoo.co.id>
2025-01-07 09:28:36 +0700

    Add seedDatabase in DatabaseChangeLog🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/mongock/config/DatabaseChangeLog.java
+++ src/main/java/id/my/hendisantika/mongock/config/DatabaseChangeLog.java
@@ -1,6 +1,18 @@
 package id.my.hendisantika.mongock.config;
 
 import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import id.my.hendisantika.mongock.model.Expense;
+import id.my.hendisantika.mongock.repository.ExpenseRepository;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static id.my.hendisantika.mongock.model.ExpenseCategory.ENTERTAINMENT;
+import static id.my.hendisantika.mongock.model.ExpenseCategory.MISC;
+import static id.my.hendisantika.mongock.model.ExpenseCategory.RESTAURANT;
+import static id.my.hendisantika.mongock.model.ExpenseCategory.UTILITIES;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +26,15 @@ import com.github.cloudyrock.mongock.ChangeLog;
  */
 @ChangeLog
 public class DatabaseChangeLog {
+    @ChangeSet(order = "001", id = "seedDatabase", author = "Sai")
+    public void seedDatabase(ExpenseRepository expenseRepository) {
+        List<Expense> expenseList = new ArrayList<>();
+        expenseList.add(createNewExpense("Movie Tickets", ENTERTAINMENT, BigDecimal.valueOf(40)));
+        expenseList.add(createNewExpense("Dinner", RESTAURANT, BigDecimal.valueOf(60)));
+        expenseList.add(createNewExpense("Netflix", ENTERTAINMENT, BigDecimal.valueOf(10)));
+        expenseList.add(createNewExpense("Gym", MISC, BigDecimal.valueOf(20)));
+        expenseList.add(createNewExpense("Internet", UTILITIES, BigDecimal.valueOf(30)));
+
+        expenseRepository.insert(expenseList);
+    }
 }
