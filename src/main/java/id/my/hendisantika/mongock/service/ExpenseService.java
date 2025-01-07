207a03cc5381af147d4f7e71a45c0faeb8fb6ff0
hendisantika <hendisantika@yahoo.co.id>
2025-01-07 09:26:39 +0700

    Add ExpenseService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/mongock/service/ExpenseService.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.mongock.service;
+
+import id.my.hendisantika.mongock.repository.ExpenseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mongock-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/01/25
+ * Time: 09.26
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+}
