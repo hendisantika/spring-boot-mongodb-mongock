7fc8581c3ae9dd379505d1353f6df1b9842c7e51
hendisantika <hendisantika@yahoo.co.id>
2025-01-07 09:24:38 +0700

    Add ExpenseCategory Enum🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/mongock/model/ExpenseCategory.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.mongock.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mongock-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/01/25
+ * Time: 09.24
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ExpenseCategory {
+    ENTERTAINMENT, GROCERIES, RESTAURANT, UTILITIES, MISC
+}
