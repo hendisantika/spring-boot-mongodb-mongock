e02271752f976f7171808473f1da9304411d160b
hendisantika <hendisantika@yahoo.co.id>
2025-01-07 09:41:25 +0700

    fix: Lombok Plugin🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/mongock/SpringBootMongockDemoApplicationTests.java
+++ src/test/java/id/my/hendisantika/mongock/SpringBootMongockDemoApplicationTests.java
@@ -1,10 +1,21 @@
 package id.my.hendisantika.mongock;
 
+import id.my.hendisantika.mongock.model.Expense;
+import id.my.hendisantika.mongock.model.ExpenseCategory;
+import id.my.hendisantika.mongock.repository.ExpenseRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Testcontainers
@@ -21,4 +32,28 @@ class SpringBootMongockDemoApplicationTests {
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
     }
 
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @Test
+    @DisplayName("Test Whether Expenses are pre-populated")
+    void shouldReturnPrePopulatedExpenses() {
+        List<Expense> expenses = expenseRepository.findAll();
+        assertEquals(5, expenses.size());
+    }
+
+    @Test
+    @DisplayName("Should Find Expense By Name")
+    void shouldFindExpenseByName() {
+        Expense expectedExpense = new Expense();
+        expectedExpense.setExpenseName("Evening Drinks");
+        expectedExpense.setExpenseCategory(ExpenseCategory.MISC);
+        expectedExpense.setExpenseAmount(BigDecimal.TEN);
+        expenseRepository.save(expectedExpense);
+
+        Expense actualExpense = expenseRepository.findByName("Evening Drinks").orElseThrow();
+        assertEquals(expectedExpense.getExpenseName(), actualExpense.getExpenseName());
+        assertEquals(expectedExpense.getExpenseCategory(), actualExpense.getExpenseCategory());
+        assertEquals(expectedExpense.getExpenseAmount(), actualExpense.getExpenseAmount());
+    }
 }
