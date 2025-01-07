e7c8c25f5f377b3898b7062fadbbaa449ed8d766
hendisantika <hendisantika@yahoo.co.id>
2025-01-07 09:30:17 +0700

    Add deleteExpense in ExpenseController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/mongock/controller/ExpenseController.java
+++ src/main/java/id/my/hendisantika/mongock/controller/ExpenseController.java
@@ -5,6 +5,7 @@ import id.my.hendisantika.mongock.service.ExpenseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,5 +53,11 @@ public class ExpenseController {
     @GetMapping("/{name}")
     public ResponseEntity getExpenseByName(@PathVariable String name) {
         return ResponseEntity.ok(expenseService.getExpense(name));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteExpense(@PathVariable String id) {
+        expenseService.deleteExpense(id);
+        return ResponseEntity.noContent().build();
     }
 }
