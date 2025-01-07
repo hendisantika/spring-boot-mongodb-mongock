2d13758adbc17dd1c0c907824d86ec301a1d3bae
hendisantika <hendisantika@yahoo.co.id>
2025-01-07 09:39:44 +0700

    Add Test Container configs🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/mongock/SpringBootMongockDemoApplicationTests.java
+++ src/test/java/id/my/hendisantika/mongock/SpringBootMongockDemoApplicationTests.java
@@ -1,13 +1,24 @@
 package id.my.hendisantika.mongock;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class SpringBootMongockDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0.4");
+
+    {
+        mongoDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
     }
 
 }
